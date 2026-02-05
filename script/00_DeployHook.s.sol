89ed6283bf3ead31129e441176530c13642ff3fc
SweetieBirdX <eyupefekarakoca@gmail.com>
2026-02-05 18:42:32 +0300

    build: update deploy script to use SentinelHook

--- script/00_DeployHook.s.sol
+++ script/00_DeployHook.s.sol
@@ -6,27 +6,30 @@ import {HookMiner} from "@uniswap/v4-periphery/src/utils/HookMiner.sol";
 
 import {BaseScript} from "./base/BaseScript.sol";
 
-import {Counter} from "../src/Counter.sol";
+import {SentinelHook} from "../src/SentinelHook.sol";
 
-/// @notice Mines the address and deploys the Counter.sol Hook contract
+/// @notice Mines the address and deploys the SentinelHook contract
 contract DeployHookScript is BaseScript {
     function run() public {
-        // hook contracts must have specific flags encoded in the address
+        // Hook contracts must have specific flags encoded in the address
+        // SentinelHook requires: beforeSwap, afterSwap, beforeAddLiquidity
         uint160 flags = uint160(
             Hooks.BEFORE_SWAP_FLAG | Hooks.AFTER_SWAP_FLAG | Hooks.BEFORE_ADD_LIQUIDITY_FLAG
-                | Hooks.BEFORE_REMOVE_LIQUIDITY_FLAG
         );
 
+        // For now, use address(0) for agentRegistry - will update when AgentRegistry is deployed
+        address agentRegistry = address(0);
+
         // Mine a salt that will produce a hook address with the correct flags
-        bytes memory constructorArgs = abi.encode(poolManager);
+        bytes memory constructorArgs = abi.encode(poolManager, agentRegistry);
         (address hookAddress, bytes32 salt) =
-            HookMiner.find(CREATE2_FACTORY, flags, type(Counter).creationCode, constructorArgs);
+            HookMiner.find(CREATE2_FACTORY, flags, type(SentinelHook).creationCode, constructorArgs);
 
         // Deploy the hook using CREATE2
         vm.startBroadcast();
-        Counter counter = new Counter{salt: salt}(poolManager);
+        SentinelHook sentinelHook = new SentinelHook{salt: salt}(poolManager, agentRegistry);
         vm.stopBroadcast();
 
-        require(address(counter) == hookAddress, "DeployHookScript: Hook Address Mismatch");
+        require(address(sentinelHook) == hookAddress, "DeployHookScript: Hook Address Mismatch");
     }
 }
